9dec38a910718ea2f04f231f5d9d3d621031aa5f
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2024-04-03 18:05:37 -0500

    Implement pickup and return forks methods.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ Purpose: Implementaation of the Dining Philosophers Problem (Project 3)
 **********************/
 #include "main.h"
 
+enum philosopherStatus {thinking, hungry, eating};
 // Setup a vector of 5 semaphores representing 5 forks
+vector<binary_semaphore*> forks;
+int NUMPHILOSOPHERS = 5;
 
 Philosopher::Philosopher() {
     philosopherId = -1;
@@ -29,6 +32,44 @@ bool Philosopher::setId(int id) {
     return success;
 }
 
+bool Philosopher::bothForksAvailable(int philosopherId) {
+    bool bothForksAvailable = false;
+    bool rightAvailable = false;
+    bool leftAvailable = forks[philosopherId]->try_acquire();
+    if(leftAvailable){
+        rightAvailable = forks[(philosopherId + 1) % NUMPHILOSOPHERS]->try_acquire();
+    }
+    if(leftAvailable && rightAvailable){
+        bothForksAvailable = true;
+    }
+    return bothForksAvailable;
+}
+
+void Philosopher::pickup_forks(int philosopherId) {
+    // Philosopher is hungry,
+    this->status = hungry;
+    // try to acquire the fork to the left and the right ONLY if BOTH AVAILABLE
+    bool forksAvailable = this->bothForksAvailable(philosopherId);
+    if(forksAvailable){
+        this->forkInLeftHand = true;
+        this->forkInRightHand = true;
+        this->status = eating;
+        cout << "Philosopher " << philosopherId << " is eating" << endl;
+        this->coursesConsumed++;
+        this->return_forks(philosopherId);
+    }
+    else{
+        this->timeSinceLastEaten++;
+    }
+}
+
+void Philosopher::return_forks(int philosopherId) {
+    this->status = thinking;
+    cout << "Philosopher " << philosopherId << " is thinking" << endl;
+    forks[philosopherId]->release();
+    forks[((philosopherId + 1) % NUMPHILOSOPHERS)]->release();
+}
+
 int randomRangeGen(int endRange, int startRange = 0, unsigned int seed = 42) {
     // General implementation borrowed from:
     // https://www.digitalocean.com/community/tutorials/random-number-generator-c-plus-plus
@@ -50,18 +91,28 @@ int randomRangeGen(int endRange, int startRange = 0, unsigned int seed = 42) {
 }
 
 int main(){
-    int numPhilosophers = 5;
+    int numCourses = 3;
+    int numForks = 5;
 
     vector<Philosopher> philosophersTable;
     // Create 5 philosophers
-
-    for(int i = 0; i < numPhilosophers; i++){
-        Philosopher philosopher;
-        philosopher.setId(i);
-        philosophersTable.push_back(philosopher);
+    for(int i = 0; i < NUMPHILOSOPHERS; i++){
+        Philosopher *philosopher = new Philosopher;
+        philosopher->setId(i);
+        philosophersTable.push_back(*philosopher);
     }
-    // Assumes 5 philosophers with 5 forks, and 2 forks required to eat (instead of chopsticks)
 
+    // Create the 5 forks (of which, each philosopher will require 1 in each hand to eat)
+    for (int i = 0; i < numForks; i++){
+        binary_semaphore *fork = new binary_semaphore(1);
+        forks.push_back(fork);
+    }
+
+    thread t1(&Philosopher::pickup_forks, philosophersTable[0], 0);
+    thread t2(&Philosopher::pickup_forks, philosophersTable[1], 1);
+
+    t1.join();
+    t2.join();
     cout << "done" << endl;
 
     return 0;
